c823b06a839404ceb6e640af3998da10b7b2073f
ngovuong.dev <ngovuong.dev@gmail.com>
2025-09-24 22:26:50 +0700

    update code

--- 240925/Vuong/studyjava/src/App.java
+++ 240925/Vuong/studyjava/src/App.java
@@ -2,6 +2,7 @@ public class App {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
         System.out.print("Hieu ko gay");
+        System.out.print("Hoang Anh Ko gay");
     }
 }
 
